d0e19d371db2affd45975078264fd816609d819f
Rob McGowan <Rob McGowan>
2016-05-08 19:51:15 -0400

    Updated cachematrix to include my code as per programming 2 assignment instructions
    Signed-off-by: Rob McGowan <Rob McGowan>

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions work in tandem to cache the inverse of a matrix
+## in a variable in a seperate environment
+## first, cache the matrix using the makeCacheMatrix function
+## then solve the matrix with the cacheSolve function
+## subsequent calls to the cacheSolve function will retrieve the value
+## of the inverse matrix
 
-## Write a short comment describing this function
+## The first function,  makeCacheMatrix  creates a list of functions, 
+## which perform the following:
+# set: set the value of the matrix in a cached variable
+# get: get the value of the matrix from the cached variable
+# setinverse: set the value of the inverse using solve() in a cached variable
+# getinverse: get the value of the inverse from the cached variable
+# 
+# during the initial call, the data is cached and a null value is entered into the 
+# cached inverse
+
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)}
 
-}
 
+## The second function cacheSolve checks to see whether there is a null value 
+## in the cached inverse value.
+## if the value is null, the inverse is calculated
+## if the value is not null, the cached inverse is returned.
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m) 
+  m
+  
 }
